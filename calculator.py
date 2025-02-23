25240ec9c086c422e62259a176b5b5a281e10295
wenvit <wendyvit@icloud.com>
2025-02-23 12:49:38 -0600

    implement feature asking user if they'd like to perform additional calcs

--- calculator.py
+++ calculator.py
@@ -2,14 +2,19 @@
 
 # Asking the user for another calculation
 
-# Currently, our calculator asks the user for two 
+# Currently, our calculator asks the user for two
 # numbers and an operation and then exits after displaying
-#  the result. Wouldn't it be nice if we could ask 
-# the user if they wanted to perform another calculation 
+#  the result. Wouldn't it be nice if we could ask
+# the user if they wanted to perform another calculation
 # and start a new calculation when they respond with yes?
 
-# code
+# algorithm
+# Add `while True:` loop that starts before prompt to enter 1st number
+#   and encloses rest of code.
+# After printing result of calculator, ask user if they'd like
+#   perform another calculation. If user inputs `no`, break
 
+# code
 def prompt(message):
     print(f'==> {message}')
 
@@ -25,40 +30,48 @@ def string_to_int(number_str):
 
 prompt('Welcome to the Calculator!')
 
-prompt('Please enter 1st number:')
-num1 = input()
+while True:
 
-while invalid_number(num1):
-    prompt('The number must be an integer. Try again')
+    prompt('Please enter 1st number:')
     num1 = input()
 
-prompt('Enter 2nd number:')
-num2 = input()
+    while invalid_number(num1):
+        prompt('The number must be an integer. Try again')
+        num1 = input()
 
-while invalid_number(num2):
-    prompt("Hmmm...that isn't a valid integer. Try again")
+    prompt('Enter 2nd number:')
     num2 = input()
 
-prompt('''Pick a mathematical operation:
-    1) addition
-    2) subtraction
-    3) multiplication
-    4) division  
-''')
-math_op = input()
+    while invalid_number(num2):
+        prompt("Hmmm...that isn't a valid integer. Try again")
+        num2 = input()
 
-while math_op not in ['1', '2', '3', '4']:
-    prompt('You must choose 1, 2, 3, or 4')
+    prompt('''Pick a mathematical operation:
+        1) addition
+        2) subtraction
+        3) multiplication
+        4) division  
+    ''')
     math_op = input()
 
-match math_op:
-    case '1':  # represents addition
-        output = int(num1) + int(num2)
-    case '2':  # represents subtraction
-        output = int(num1) - int(num2)
-    case '3':  # represents multiplication
-        output = int(num1) * int(num2)
-    case '4':  # represents division
-        output = int(num1) / int(num2)
+    while math_op not in ['1', '2', '3', '4']:
+        prompt('You must choose 1, 2, 3, or 4')
+        math_op = input()
 
-prompt(f'The result is {output}')
+    match math_op:
+        case '1':  # represents addition
+            output = int(num1) + int(num2)
+        case '2':  # represents subtraction
+            output = int(num1) - int(num2)
+        case '3':  # represents multiplication
+            output = int(num1) * int(num2)
+        case '4':  # represents division
+            output = int(num1) / int(num2)
+
+    prompt(f'The result is {output}')
+
+    prompt('Would you like to perform another calculation?')
+    user_response = input()
+
+    if user_response not in ['y', 'Yes', 'yes']:
+        break
